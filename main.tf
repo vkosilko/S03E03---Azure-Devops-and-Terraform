b74c9b11228284f7f2d26a4229019a591583895b
unknown <Admin@49114628v01.de.ema.ad.pwcinternal.com>
2021-07-31 21:28:08 +0200

    changed the tf state

--- main.tf
+++ main.tf
@@ -3,45 +3,38 @@ provider "azurerm" {
     features {}
 }
 
-terraform {
-    backend "azurerm" {
-        resource_group_name  = "tf_rg_blobstore"
-        storage_account_name = "tfstoragebinarythistle"
-        container_name       = "tfstate"
-        key                  = "terraform.tfstate"
-    }
+resource "azurerm_resource_group" "test_tf" {
+  name = "test_tf"
+  location = "northeurope"
 }
 
-variable "imagebuild" {
-  type        = string
-  description = "Latest Image Build"
+resource "azurerm_container_registry" "terraformcr" {
+  name                     = "terraformcr"
+  resource_group_name      = azurerm_resource_group.test_tf.name
+  location                 = azurerm_resource_group.test_tf.location
+  admin_enabled            = true
+  sku                      = "Premium"
 }
 
 
+resource "azurerm_container_group" "test_tfcg" {
+  name                  = "weatherapi"
+  location              = azurerm_resource_group.test_tf.location
+  resource_group_name   = azurerm_resource_group.test_tf.name
 
-resource "azurerm_resource_group" "tf_test" {
-  name = "tfmainrg"
-  location = "Australia East"
-}
-
-resource "azurerm_container_group" "tfcg_test" {
-  name                      = "weatherapi"
-  location                  = azurerm_resource_group.tf_test.location
-  resource_group_name       = azurerm_resource_group.tf_test.name
-
-  ip_address_type     = "public"
-  dns_name_label      = "binarythistlewa"
-  os_type             = "Linux"
+  ip_address_type       = "public"
+  dns_name_label        = "binarythisleapifewa"
+  os_type               = "Linux"
 
   container {
-      name            = "weatherapi"
-      image           = "binarythistle/weatherapi:${var.imagebuild}"
-        cpu             = "1"
-        memory          = "1"
+    name             = "weatherapi"
+    image            = "terraformcr.azurecr.io/weatherapi:latest"
+      cpu               = "1"
+      memory            = "1"
 
-        ports {
-            port        = 80
-            protocol    = "TCP"
-        }
+      ports {
+        port            = "80"
+        protocol        = "TCP"
+      }
   }
 }
