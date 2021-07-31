cdfb20b9b9e622c1795a0374334909c054574218
unknown <Admin@49114628v01.de.ema.ad.pwcinternal.com>
2021-08-01 01:39:22 +0200

    remove creation CR

--- main.tf
+++ main.tf
@@ -18,14 +18,6 @@ resource "azurerm_resource_group" "test_tf" {
   location = "northeurope"
 }
 
-resource "azurerm_container_registry" "terraformcr" {
-  name                     = "terraformcr"
-  resource_group_name      = azurerm_resource_group.test_tf.name
-  location                 = azurerm_resource_group.test_tf.location
-  admin_enabled            = true
-  sku                      = "Premium"
-}
-
 
 resource "azurerm_container_group" "test_tfcg" {
   name                  = "weatherapi"
